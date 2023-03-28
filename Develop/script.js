07070c534ca555db2fec4aee23ecf60a14d0edd4
unknown <dbp1795@aim.com>
2023-03-28 09:39:03 -0400

    resolved issue, had to add 'beforeunload' function to keep data on page after leaving

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,59 @@
-
-
-  $(function () {
-    // Add click event listener to all save buttons
-    $(".saveBtn").on("click", function() {
-      // Get the id of the containing time-block
-      var blockId = $(this).closest(".time-block").attr("id");
-      // Get the user input from the description text area
-      var userInput = $(this).siblings(".description").val();
-      // Save the user input in local storage using the block id as the key
-      localStorage.setItem(blockId, userInput);
-    });
+// Add event listener to all save buttons
+function addSaveButtonListeners() {
+  $(".saveBtn").each(function () {
+    $(this).on("click", saveUserInput.bind(this));
   });
-  $(function () {
-    // Get current date and time
-    var currentHour = dayjs().hour();
-  
-    // Loop through each time block
-    $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  
-      // Apply appropriate class based on comparison to current hour
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } else if (blockHour === currentHour) {
-        $(this).addClass("present");
-      } else {
-        $(this).addClass("future");
-      }
-    });
-  
-    // Add listener for click events on save button
-    $(".saveBtn").on("click", function () {
-      // Get user input
-      var userInput = $(this).siblings(".description").val().trim();
-      // Get id of time block as key for local storage
-      var blockId = $(this).parent().attr("id");
-      // Save user input to local storage
-      localStorage.setItem(blockId, userInput);
-    });
+}
+$(window).on('beforeunload', function() {
+  $(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    var userInput = $(this).find(".description").val();
+    localStorage.setItem(blockId, userInput);
   });
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-;
+});
+
+function saveUserInput(btn) {
+  var blockId = $(btn).closest(".time-block").attr("id");
+  var userInput = $(btn).siblings(".description").val();
+  localStorage.setItem(blockId, userInput); // save the user input in local storage
+}
+
+// Apply appropriate class based on comparison to current hour
+function applyTimeBlockClasses() {
+  var currentHour = dayjs().hour();
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
+
+// Get saved user input from local storage
+function getSavedUserInput() {
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    var savedUserInput = localStorage.getItem(timeBlockId); // get saved user input from local storage
+    if (savedUserInput !== null) {
+      $(this).find(".description").val(savedUserInput); // set the value of the textarea to the saved user input
+    }
+  });
+}
+
+// Display current date in header of page
+function displayCurrentDate() {
+  var currentDate = dayjs().format("dddd, MMMM D");
+  document.getElementById("currentDay").textContent = currentDate;
+}
+
+// Call functions on page load
+$(document).ready(function() {
+  addSaveButtonListeners();
+  applyTimeBlockClasses();
+  getSavedUserInput(); // populate textareas with saved user input
+  displayCurrentDate();
+});
